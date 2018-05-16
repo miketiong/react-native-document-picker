9de715968547906e1a6b6f5ca767cd758a3bcf15
miketiong <miketiongky.90@gmail.com>
2018-05-16 17:17:32 +0800

    remove unused override

--- android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
@@ -14,11 +14,6 @@ import java.util.List;
 public class ReactNativeDocumentPicker implements ReactPackage {
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
